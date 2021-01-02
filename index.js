2101cc3d45611ed829e35f9069a404fb37b8ed6b
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-01-02 15:26:01 -0300

    finalizing CRUD API

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const validation = (course) =>{
     const schema =Joi.object({ name: Joi.string() .min(2) .required()})
     return schema.validate(course)
 }
-//handling GET http methods
+//handling GET  requests
 app.get('/', (req, res) => {
   res.send('server is just ready');
 });
@@ -25,17 +25,20 @@ app.get('/api/courses', (req, res) => {
 
   app.get('/api/courses/:id', (req, res) => {
     const course  = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) res.status(404).send('The course was not found')
+    if (!course){
+        res.status(404).send('The course was not found')
+        return
+    } 
     else res.send(course)
   })
 
 
-//handling get http methods
+//handling POST requests
   app.post('/api/courses',(req,res) =>{
 
-    const result = validation(req.body)
-    if(result.error) {
-        res.status(400).send(result.error.details[0].message)
+    const {error} = validation(req.body)
+    if(error) {
+        res.status(400).send(error.details[0].message)
         return;
     }
     
@@ -48,22 +51,34 @@ app.get('/api/courses', (req, res) => {
       res.send(course);
   })
 
-//handling PUT http methods
+//handling PUT requests
   app.put('/api/courses/:id',(req,res) =>{
     const course  = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) res.status(404).send('The course was not found')
-    const result = validation(req.body)
-    if(result.error) {
-        res.status(400).send(result.error.details[0].message)
+    if (!course){
+        res.status(404).send('The course was not found')
+        return
+    } 
+    const {error} = validation(req.body)
+    if(error) {
+        res.status(400).send(error.details[0].message)
         return;
     }
     course.name = req.body.name
     res.send(course)    
 
 })
-
+//handling DELETE requests
 app.delete('/api/courses/:id',(req,res) =>{
+    const course  = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course){
+        res.status(404).send('The course was not found')
+        return
+    } 
+    const index = courses.indexOf(course)
 
+    courses.splice(index, 1)
+
+    res.send(course)
 })
 app.listen(port, () => {
   console.log(`server at http://localhost:${port} ... `);
