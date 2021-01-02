8dcb41243e8e8f68eb13d3f8d0cfc413e785c0b2
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-01-02 13:34:53 -0300

    handling get requests

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 
+
+
 const app = express();
+app.use(express.json())
 const port = process.env.PORT || 5000;
 const courses = [
     {id:1,name:"Pescaria"},
@@ -11,13 +14,21 @@ app.get('/', (req, res) => {
   res.send('server is just ready');
 });
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3]);
-  })
+    res.send(courses);
+  });
 
   app.get('/api/courses/:id', (req, res) => {
     const course  = courses.find(c => c.id === parseInt(req.params.id))
     if (!course) res.status(404).send('The course was not found')
     else res.send(course)
+  })
+  app.post('/api/courses',(req,res) =>{
+      const course = {
+          id: courses.length + 1,
+          name:res.body.name
+      }
+      cousers.push(course)
+      res.send(course);
   })
 app.listen(port, () => {
   console.log(`server at http://localhost:${port} ... `);
